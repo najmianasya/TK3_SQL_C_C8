0e926fd0966a30b1bc01aede4caea4c3172ef3bc
sherlockjack <sherlocknagata@gmail.com>
2023-05-07 11:49:58 +0700

    add history rapat

--- project_django/urls.py
+++ project_django/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('auth/',include('authentication.urls'))
+    path('auth/',include('authentication.urls')),
+    path('history',include('history_rapat.urls'))
 ]
