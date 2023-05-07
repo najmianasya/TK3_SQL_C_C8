f6ff5811e54f0308e21fb25c635329a6ec38b9eb
Najmi Anasya Calyla <najmi.anasya@ui.ac.id>
2023-05-07 13:27:29 +0700

    Fix routings

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from dashboard.views import show_dashboard
+from dashboard.views import *
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', show_dashboard, name='show_dashboard'),
+    path('manajer/', show_dashboard_manajer, name='show_dashboard_manajer'),
+    path('penonton/', show_dashboard_penonton, name='show_dashboard_penonton'),
+    path('panitia/', show_dashboard_panitia, name='show_dashboard_panitia'),
 ]
